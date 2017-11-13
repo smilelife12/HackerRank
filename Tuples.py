d0834b6b8c9348c2ea2d768d697d0b02e5415eba
chocoberry <smilelife12@gmail.com>
2017-11-13 15:10:14 +0900

    11/13

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    # n= int(input())
+    # integer_list = map(int, input().split())
+    # # print(integer_list)
+    # t = tuple(integer_list)
+    # print (hash(t))
+    print(list(map(lambda a : a*2, [1,2,3])))
+
+# map(f,iterable) do return result of iterable types excuted 'f'function
+# and map's return type is map object means when you need to print map you should change type
